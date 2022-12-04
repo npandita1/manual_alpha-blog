be46e0eaae8d87c58b2731f30a11d6c281f853a3
npandit1 <npandita1@paychex.com>
2022-12-04 22:34:57 +0000

    Flash and Error messages completed and Edit and Update actions completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,9 +14,24 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(params.require(:article).permit(:title, :description)) 
         if @article.save
+            flash[:notice] = "Article was successfully saved."
             redirect_to article_path(@article)
         else
             render 'new', status: :unprocessable_entity
         end        
+    end    
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was successfully updated."
+            redirect_to article_path(@article)
+        else
+            render 'edit', status: :unprocessable_entity
+        end       
     end    
 end    
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
