558885c27cd340ddb62431227ac1ab242019f6ed
npandit1 <npandita1@paychex.com>
2022-12-04 22:04:08 +0000

    New and Create actions completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,18 @@ class ArticlesController < ApplicationController
     
     def index
         @articles = Article.all
+    end
+    
+    def new
+        @article = Article.new
+    end 
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description)) 
+        if @article.save
+            redirect_to article_path(@article)
+        else
+            render 'new', status: :unprocessable_entity
+        end        
     end    
 end    
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
