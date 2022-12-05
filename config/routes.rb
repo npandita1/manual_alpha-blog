ef9cd51c3bb87d57d6d71b4141d0af6fc617f46f
npandit1 <npandita1@paychex.com>
2022-12-05 07:55:59 +0000

    Delete action(with pop-up) Completed and UI clean up done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
+  root 'articles#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
